6d54e71e57222dd44d0815504833ca53cc2aba7f
MiguelMonederoRubio <miguelmonedero1996@gmail.com>
2022-04-16 18:52:12 +0200

    visualization

--- streamlit/myapp.py
+++ streamlit/myapp.py
@@ -10,17 +10,33 @@ import numpy as np
 import altair as alt
 import folium as folium  # pip install folium
 from streamlit_folium import folium_static # pip install streamlit-folium
-
+from folium.features import GeoJson, GeoJsonTooltip, GeoJsonPopup
 
 # wide mode
 st.set_page_config(layout="wide")
 
-#st.title('Medicine Selectivity Scores App')
+# LAYING OUT THE TOP SECTION OF THE APP
+row1_1, row1_2 = st.columns((2, 2))
 
-st.write("""
-# Medicine Selectivity Scores App
-Display Scores of Selected University and Year(s)
-""")
+with row1_1:
+    st.title("Medicine Selectivity Scores App")
+    
+with row1_2:
+    st.write(
+        """
+    ##
+    Displayed are the scores of public universities in Spain to study medicine (selectivity scores).
+    "1_list" is the first grade to enter the univeristy and "final_grade" the last one, therefore the lowest one.
+    By sliding the slider on the left you can the different scores per year.
+    """
+    )
+
+row2_1, row2_2 = st.columns((2, 2))
+
+with row2_1:
+  selected_year = st.slider("Select year", 2010, 2022)
+
+
 
 dfs_unified_orig = pd.read_csv('/Users/miguel/repos/Prediction_Medicine_Selectivity_Scores/output/exported_data.csv')
 
@@ -34,63 +50,167 @@ dfs_unified = dfs_unified_orig[['year', 'university', '1_list', 'final_grade']]
 #selected_year = st.sidebar.multiselect('Year', reversed(dfs_unified['year'].unique()), reversed(dfs_unified['year'].unique()))
 #selected_year = st.sidebar.select_slider('Slide to select period of time', options=['2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021','2022'])
 
-# Sidebar - University selection
-#selected_university = st.sidebar.selectbox('University', sorted(dfs_unified['university'].unique()))
+convocatory = ['final_grade', '1_list']
+with row2_2:
+  convocatory_selected = st.radio('Select  convocatory', convocatory)
 
-# Filtering data
-#df_selected_university_year = dfs_unified[(dfs_unified['university'] == selected_university) & (dfs_unified['year'].isin(selected_year))]
+
+
 #df_selected_university_year = df_selected_university_year.sort_values(by=['year'], ascending=False)
 
 #st.write('Data Dimension: ' + str(df_selected_university_year.shape[0]) + ' rows and ' + str(df_selected_university_year.shape[1]) + ' columns.')
 
 #st.dataframe(df_selected_university_year, 1000, 2000)
 
-prueba = pd.read_csv('/Users/miguel/repos/Prediction_Medicine_Selectivity_Scores/output/prueba.csv')
-
-json1 = f"/Users/miguel/repos/Prediction_Medicine_Selectivity_Scores/georef-spain-comunidad-autonoma.geojson"
-
-ma = folium.Map(location=[40,-4], zoom_start=6, width=700, height=600, control_scale=True, tiles='CartoDB Positron',
-               name='Light Map', attr='My Data attribution')
-
-choice = ['final_grade', '1_list']
-choice_selected = st.selectbox('Select  convocatory', choice)
-
-folium.Choropleth(
-    geo_data=json1,
-    name='choropleth',
-    data=prueba,
-    columns=['acom_code',choice_selected],
-    key_on='feature.properties.acom_code',
-    fill_color='YlGn',
-    fill_opacity=0.5,
-    line_opacity=0.2,
-    legend_name=choice_selected
-).add_to(ma)
-
-#folium.LayerControl().add_to(ma)
-
-folium_static(ma, width=700, height=600)
 
 
 
 
 
+# LAYING OUT THE MIDDLE SECTION OF THE APP WITH THE MAPS
+row2_1, row2_2 = st.columns((2, 2))
+
+df_selected_year = dfs_unified[(dfs_unified['year'] == selected_year)]
+
+with row2_1:
+
+# bar chart
+  if convocatory_selected == '1_list':
+    chart = alt.Chart(df_selected_year).mark_bar().encode(
+        y=alt.Y('university:O', sort=alt.EncodingSortField(field="1_list", op="values", order='descending'), axis=alt.Axis(title='University')),
+        x=alt.X('1_list:Q', axis=alt.Axis(title='1_list'), scale=alt.Scale(domain=(min(df_selected_year['1_list']) - 0.01, max(df_selected_year['1_list']) + 0.01))),
+        #color=alt.Color('convocatory:N'),
+        tooltip=('university','1_list')
+      ).properties(title= str(selected_year) + ' 1_list scores', width=200, height=600)
+    st.altair_chart(chart, use_container_width=True)
+  else:
+    chart = alt.Chart(df_selected_year).mark_bar().encode(
+        y=alt.Y('university:O', sort=alt.EncodingSortField(field="final_grade", op="values", order='descending'), axis=alt.Axis(title='University')),
+        x=alt.X('final_grade:Q', scale=alt.Scale(domain=(min(df_selected_year['final_grade']) - 0.01, max(df_selected_year['final_grade']) + 0.01))),
+        #color=alt.Color('convocatory:N'),
+        tooltip=('university','final_grade')
+      ).properties(title= str(selected_year) + ' final_grade scores', width=200, height=600)
+    st.altair_chart(chart, use_container_width=True)
 
 
 
-#df_selected_university_year = df_selected_university_year.melt(id_vars=['year'], value_vars=['1_list', 'final_grade'],
-        #var_name='convocatory', value_name='score')
+with row2_2:
+  prueba = pd.read_csv('/Users/miguel/repos/Prediction_Medicine_Selectivity_Scores/output/prueba.csv')
+  json1 = f"/Users/miguel/repos/Prediction_Medicine_Selectivity_Scores/georef-spain-comunidad-autonoma.geojson"
+  m = folium.Map(location=[37,-8], zoom_start=5, width=700, height=550, control_scale=True, tiles='CartoDB Positron',
+                name='Light Map', attr='My Data attribution')
+  
 
-# line chart 1_list and final_grade
-'''
-chart = alt.Chart(df_selected_university_year).mark_line(point=True).encode(
-  x=alt.X('year:N', axis=alt.Axis(title='Year')),
-  y=alt.Y('score:Q', axis=alt.Axis(title='Score'), scale=alt.Scale(domain=(min(df_selected_university_year['score']), 14))),
-  color=alt.Color('convocatory:N'),
-  tooltip='score'
-).properties(title= str(selected_university) + ' evolution of 1_list and final_grade scores across years')
-st.altair_chart(chart, use_container_width=True)
-'''
+
+  #Add layers for Popup and Tooltips
+
+  popup = GeoJsonPopup(
+      fields=['acom_name'],
+      aliases=['CCAA'], 
+      localize=True,
+      labels=True,
+      style="background-color: yellow;",
+  )
+
+  tooltip = GeoJsonTooltip(
+      fields=['acom_name'],
+      aliases=['CCAA'],
+      localize=True,
+      sticky=False,
+      labels=True,
+      style="""
+          background-color: #F0EFEF;
+          border: 1px solid black;
+          border-radius: 3px;
+          box-shadow: 3px;
+      """,
+      max_width=700,
+  )
+
+
+
+  # Choropleth layer
+  g = folium.Choropleth(
+      geo_data=json1,
+      name='choropleth',
+      data=prueba,
+      columns=['acom_code',convocatory_selected],
+      key_on='feature.properties.acom_code',
+      fill_color='YlGn',
+      fill_opacity=0.5,
+      line_opacity=0.2,
+      legend_name=convocatory_selected,
+      highlight=True
+  ).add_to(m)
+
+
+  folium.GeoJson(
+      json1,
+      style_function=lambda feature: {
+          'fillColor': '#ffff00',
+          'color': 'black',
+          'weight': 0.2,
+          'dashArray': '5, 5'
+      },
+      tooltip=tooltip,
+      popup=popup).add_to(g)
+
+  folium.LayerControl().add_to(m)
+  folium_static(m, width=700, height=550)
+  
+
+
+
+
+# LAYING OUT THE TOP SECTION OF THE APP
+row3_1, row3_2 = st.columns((2, 2))
+
+with row3_1:
+    
+# University selection
+  selected_university = st.selectbox('Choose an university to view the convocatory selected above across years', sorted(dfs_unified['university'].unique()))
+
+with row3_2:
+    st.write(
+        """
+    ##
+    Choose an university to view the scores across years and the correlation heatmap
+    """
+    )
+
+# LAYING OUT THE MIDDLE SECTION OF THE APP WITH THE MAPS
+row4_1, row4_2 = st.columns((2, 2))
+
+
+def line_chart_selected_uni(uni_name_selected, convocatory):
+  #create y_axis name
+  if convocatory == 'final_grade':
+      y_axis = 'final_grade:Q'
+  else:
+      y_axis = '1_list:Q'
+  # filter by university
+  df_selected_university = dfs_unified[(dfs_unified['university'] == uni_name_selected)]
+  # chart
+  chart = alt.Chart(df_selected_university).mark_line(point=True).encode(
+      x=alt.X('year:N', axis=alt.Axis(title='Year')),
+      y=alt.Y(y_axis, axis=alt.Axis(title=convocatory), scale=alt.Scale(domain=(min(df_selected_university[convocatory]) - 0.01, max(df_selected_university[convocatory]) + 0.01))),
+      tooltip=convocatory).properties(title= str(uni_name_selected) + ' evolution of ' + str(convocatory) + ' scores across years')
+  return st.altair_chart(chart, use_container_width=True)
+
+
+
+
+with row4_1:
+
+  #df_selected_university_year = dfs_unified[(dfs_unified['university'] == selected_university) & (dfs_unified['year'] == selected_year)]
+  #df_selected_university_year = df_selected_university_year.melt(id_vars=['year'], value_vars=['1_list', 'final_grade'],
+          #var_name='convocatory', value_name='score')
+
+
+  # line chart 1_list and final_grade
+  line_chart_selected_uni(selected_university, convocatory_selected)
+ 
+
 
 # line chart final_grade
 #chart = alt.Chart(df_selected_university_year).mark_line(point=True).encode(
@@ -101,20 +221,13 @@ st.altair_chart(chart, use_container_width=True)
 #).properties(title='Evolution of final_grade scores across years').interactive()
 #st.altair_chart(chart, use_container_width=True)
 
-'''
-# Heatmap
-if st.button('Intercorrelation Heatmap'):
-    st.header('Intercorrelation Matrix Heatmap')
-    df_selected_university_year[['year', '1_list', 'final_grade']].to_csv('output.csv',index=False)
-    df = pd.read_csv('output.csv')
 
-    corr = df.corr()
-    mask = np.zeros_like(corr)
-    mask[np.triu_indices_from(mask)] = True
-    with sns.axes_style("white"):
-        f, ax = plt.subplots(figsize=(7, 5))
-        ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
-    st.pyplot()
-st.set_option('deprecation.showPyplotGlobalUse', False)
-'''
+with row4_2:
+  df_selected_university = dfs_unified[(dfs_unified['university'] == selected_university)]
+  df = df_selected_university[['year', '1_list', 'final_grade']]
+  plt.figure(figsize=(16, 6))
+  heatmap = sns.heatmap(df.corr(), vmax=1, annot=True, cmap='BrBG')
+  heatmap.set_title('Correlation Heatmap', fontdict={'fontsize':12}, pad=12);
+  st.pyplot()
+
 
